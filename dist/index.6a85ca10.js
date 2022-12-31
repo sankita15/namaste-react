d9f4d8cf49b25a6ec8c2968106ee1502b8c96867
Ankita Srivastava <ankitacs0018@gmail.com>
2022-12-31 19:04:47 +0100

    Add script for start and building the app

---
+++ dist/index.6a85ca10.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", null, "Hello World by React modularized");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.6a85ca10.js.map
