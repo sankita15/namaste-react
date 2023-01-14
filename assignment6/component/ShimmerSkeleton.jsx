631dbb6fc00bd441d72f8bf6469734fa6c80fe64
Ankita Srivastava <ankitacs0018@gmail.com>
2023-01-14 22:06:55 +0100

    Update shimmer content width

--- assignment6/component/ShimmerSkeleton.jsx
+++ assignment6/component/ShimmerSkeleton.jsx
@@ -5,8 +5,8 @@ const ShimmerSkeleton = () => {
     return (
         <div className="shimmer-restaurant-container">
             <div className="shimmer-image-background"/>
-            <div className="shimmer-content" />
-            <div className="shimmer-content" />
+            <div className="shimmer-main-content content1" />
+            <div className="shimmer-main-content content2" />
             <div className="shimmer-restaurant-info-container">
                 <div className="shimmer-sub-content" />
                 <div className="shimmer-separator">•</div>
